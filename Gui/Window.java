050e44f5340840c483e5c0eb0a682940a4a8531a
yoaveliashiv <57868000+yoaveliashiv@users.noreply.github.com>
2019-12-30 20:18:52 +0200

    Update Window.java

--- Gui/Window.java
+++ Gui/Window.java
@@ -13,6 +13,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -24,8 +25,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-
-
+import Tester.all;
 import algorithms.Graph_Algo;
 import dataStructure.DGraph;
 import dataStructure.Edgedata;
@@ -35,29 +35,24 @@ import dataStructure.graph;
 import dataStructure.node_data;
 import utils.Point3D;
 
-public class Window extends JFrame implements ActionListener, MouseListener
-{
+public class Window extends JFrame implements ActionListener, MouseListener {
 	DGraph g0= new DGraph();
+	
 	ArrayList<node_data> g3= new ArrayList<node_data>();
 	ArrayList<Integer> g4= new ArrayList<Integer>();
 	String w="";
 	int src=0;
 	int dest=0;
 	int count=0;
-	public Window()
-	{
-
+	int countSave=0;
+	public Window(){
 		initGUI();
 	}
-
-	private void initGUI() 
-	{
-
-		this.setSize(1000, 600);
+	private void initGUI() {
+		this.setSize(600, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		MenuBar menuBar = new MenuBar();
-
+		
 		Menu menu = new Menu("Menu");
 		menuBar.add(menu);
 		this.setMenuBar(menuBar);
@@ -67,7 +62,7 @@ public class Window extends JFrame implements ActionListener, MouseListener
 		menuBar.add(algo);
 		this.setMenuBar(menuBar);
 
-		MenuItem item1 = new MenuItem("save");
+		MenuItem item1 = new MenuItem("savePic");
 		item1.addActionListener(this);
 
 		MenuItem item2 = new MenuItem("clearAll");
@@ -94,10 +89,14 @@ public class Window extends JFrame implements ActionListener, MouseListener
 
 		MenuItem item7 = new MenuItem("clearWay");
 		item7.addActionListener(this);
+		
+		MenuItem item10 = new MenuItem("saveGraph");
+		item10.addActionListener(this);
 
 		menu.add(item1);
 		menu.add(item2);
 		menu.add(item7);
+		menu.add(item10);
 		algo.add(item3);
 		algo.add(item4);
 		algo.add(item5);
@@ -108,22 +107,13 @@ public class Window extends JFrame implements ActionListener, MouseListener
 		
 	}
 	public void paint(Graphics g) {
-		{
 			super.paint(g);
-
 			ArrayList<node_data> g1= new ArrayList<node_data>(g0.getV());
 			g.setColor(Color.BLUE);
-			for (node_data p: g1) 
-			{
+			for (node_data p: g1) {
 				g.fillOval((int) p.getLocation().x(), (int) p.getLocation().y(), 7,7);	
-				String str= Integer.toString(p.getKey());      ///מה עושה????
+				String str= Integer.toString(p.getKey());      
 				g.drawString(str, (int)p.getLocation().x(), (int)p.getLocation().y()  );
-			//	double px=p.getLocation().x()*100;
-			///	double pxe=(int)px;
-				//double py=p.getLocation().y()*100;
-				//double pye=(int)py;
-				//String str1="("+pxe/100+","+pye/100+")" ;      ///מה עושה????
-			//	g.drawString(str1, (int)p.getLocation().x()-30, (int)p.getLocation().y()-10  );
 			}
 			for(Edgedata p : g0.geta()) {
 				g.setColor(Color.ORANGE);
@@ -138,7 +128,7 @@ public class Window extends JFrame implements ActionListener, MouseListener
 				g.setColor(Color.RED);
 				g.drawString(str,(int)((g1.get(p.getSrc()).getLocation().x()+g1.get(p.getDest()).getLocation().x())/2),(int)((g1.get(p.getSrc()).getLocation().y()+g1.get(p.getDest()).getLocation().y())/2));
 			}
-		}
+		
 		g.setColor(Color.black);
 		for (int i = 0; i <g3.size()-1; i++) {
 			g.drawLine(g3.get(i).getLocation().ix(),g3.get(i).getLocation().iy(),g3.get(i+1).getLocation().ix(),g3.get(i+1).getLocation().iy());		
@@ -149,136 +139,100 @@ public class Window extends JFrame implements ActionListener, MouseListener
 	public void actionPerformed(ActionEvent e) 
 	{
 		String str = e.getActionCommand();
-
-		if(str.equals("save"))
-		{
-			Graph_Algo a = new Graph_Algo();
+		Graph_Algo a = new Graph_Algo();
+		if(str.equals("savePic"))	{
 			a.init(g0);
 			save_paint();
-			
 		}
-		if(str.equals("clearAll"))
-		{
+		if(str.equals("clearAll")){
 			w="";
 			count=0;
 			src=0;
 			dest=0;
 			g3=new ArrayList<node_data>();
 			g0=new DGraph();
+			g4 = new ArrayList<Integer>();
 		}
-		if(str.equals("clearWay"))
-		{
+		if(str.equals("clearWay")){
 			w="";
 			count=0;
 			src=0;
 			dest=0;
 			g3=new ArrayList<node_data>();
+			g4 = new ArrayList<Integer>();
 		}
 
-		if(str.equals("isconnected"))
-		{
-			Graph_Algo a = new Graph_Algo();
+		if(str.equals("isconnected")){
 			a.init(g0);
 			System.out.println(a.isConnected());
 		}
-		if(str.equals("shortestPathDist"))
-		{
-			System.out.println("in");
-			Graph_Algo a = new Graph_Algo();
+		if(str.equals("shortestPathDist"))	{
 			a.init(g0);
 			System.out.println(a.shortestPathDist(0,4));
 		}
-		if(str.equals("path"))
-		{
+		if(str.equals("path")){
 			w=str;
-			
-
 		}
 		if(str.equals("endPath")) {
-			Graph_Algo a = new Graph_Algo();
 			a.init(g0);
 			g3= new ArrayList<node_data>(a.shortestPath(src, dest));
-
 			ArrayList<Integer> g2 = new ArrayList<Integer>();
-			for (int i = 0; i < g3.size(); i++) {	
-				g2.add(g3.get(i).getKey());	
-			}	
+			for (int i = 0; i < g3.size(); i++) 		g2.add(g3.get(i).getKey());	
+			System.out.println(g2);
 			w="";
 			count=0;
 			src=0;
 			dest=0;
-			System.out.println(g2);
 		}
-		if(str.equals("endTSP")) {
-			Graph_Algo a = new Graph_Algo();
+		if(str.equals("endTSP")) {	
 		a.init(g0);
-		
 		g3= new ArrayList<node_data>(a.TSP(g4));
 		ArrayList<Integer>  g2 = new ArrayList<Integer>();
-		for (int i = 0; i < g3.size(); i++) {	
-			g2.add(g3.get(i).getKey());	
-		}	
-		g4 = new ArrayList<Integer>();
+		for (int i = 0; i < g3.size(); i++) 	g2.add(g3.get(i).getKey());	
 		System.out.println(g2);
+		g4 = new ArrayList<Integer>();
 			w="";
-			
 		}
-		if(str.equals("TSP"))
-		{
+		if(str.equals("TSP")){
 		w=str;
-		
-			
-
-			/**code to complete
-			 try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			 */
 		}
-
+		if(str.equals("saveGraph")) {
+			a.init(g0);
+			a.save("graph"+countSave+".txt");
+			countSave++;
+		}
 		repaint();
 	}
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println("mouseClicked");	
+		//System.out.println("mouseClicked");	
 	}
-	
 	
 	@Override
 	public void mousePressed(MouseEvent e) {
 		if(w.equals("path")) {
 			double x = e.getX();
 			double y = e.getY();
-			Point3D p = new Point3D(x,y);
 				ArrayList<node_data> g1= new ArrayList<node_data>(g0.getV());
-
 				for(node_data a: g1) {
-					Point3D p1 = new Point3D((int)a.getLocation().x(),(int)y);
-					//System.out.println(p1);
+					Point3D p1 = new Point3D((int)a.getLocation().x(),(int)a.getLocation().y());
 					if (x<p1.x()+15 && x>p1.x()-15 && y<p1.y()+15 && y>p1.y()-15) {
 						if(count==0)src=a.getKey();
 						else dest=a.getKey();
 					}
 				}
 				count++;
-			System.out.println("in list: "+src+","+dest);
-			
+		if(count>1)	System.out.println("in list: "+src+","+dest);
 		}
 		if(w.equals("TSP")) {
 			double x = e.getX();
 			double y = e.getY();
-			 new Point3D(x,y);
 				ArrayList<node_data> g1= new ArrayList<node_data>(g0.getV());
-
 				for(node_data a: g1) {
-					Point3D p1 = new Point3D((int)a.getLocation().x(),(int)y);
-				
+					Point3D p1 = new Point3D((int)a.getLocation().x(),(int)a.getLocation().y());
 					if (x<p1.x()+15 && x>p1.x()-15 && y<p1.y()+15 && y>p1.y()-15) g4.add(a.getKey());
 				}
 			System.out.println("in list:"+g4.get(g4.size()-1));
-			
 		}
 		else {
 		int x = e.getX();
@@ -289,14 +243,14 @@ public class Window extends JFrame implements ActionListener, MouseListener
 			Point3D p = new Point3D(x,y);
 			int i= g0.nodeSize();
 			Nodedata b=new Nodedata(p,i);
+			System.out.println("new node:"+b.getKey());
 			g0.addNode(b);
 			repaint();		
 		}	
 		else {
 			for(node_data a: g1) {
 				if (x<a.getLocation().x()+15 && x>a.getLocation().x()-15 && y<a.getLocation().y()+15 && y>a.getLocation().y()-15) { 	
-					g0.setnode(x,y).setWeight(1);
-					System.out.println(g0.setnode(x,y).getWeight());		
+					g0.setnode(x,y).setWeight(1);	
 				}
 				else {	
 					j++;			
@@ -304,6 +258,7 @@ public class Window extends JFrame implements ActionListener, MouseListener
 						Point3D p = new Point3D(x,y);
 						int i= g0.nodeSize();
 						Nodedata b=new Nodedata(p,i);
+						System.out.println("new node:"+b.getKey());
 						g0.addNode(b);
 						repaint();
 						break;
@@ -311,12 +266,10 @@ public class Window extends JFrame implements ActionListener, MouseListener
 				}
 			}
 		}
-		System.out.println("mousePressed");
 		}
 	}
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
 		int x = e.getX();
 		int y = e.getY();
 		Point3D p = new Point3D(x,y);
@@ -327,7 +280,10 @@ public class Window extends JFrame implements ActionListener, MouseListener
 					for(node_data a: g1) {
 						if(a.getWeight()==1) {
 							if(g0.setnode(x,y)==a) {}
-							else{g0.connect(a.getKey(),g0.setnode(x,y).getKey(),(p.distance2D(a.getLocation())));}
+							else{
+								g0.connect(a.getKey(),g0.setnode(x,y).getKey(),g0.getNode(g0.setnode(x,y).getKey()).getLocation().distance2D(a.getLocation()));
+								System.out.println("concet:"+a.getKey()+" to:"+g0.setnode(x,y).getKey());
+							}
 						}
 						a.setWeight(999999999);	
 					}	
@@ -335,10 +291,8 @@ public class Window extends JFrame implements ActionListener, MouseListener
 				repaint();		
 			}
 		}
-		System.out.println("mouseReleased");
 		}
 
-	
 	@Override
 	public void mouseEntered(MouseEvent e) {
 	
@@ -360,20 +314,4 @@ public class Window extends JFrame implements ActionListener, MouseListener
 		{System.out.println("no image");
 		}
 	}
-	public void create() {
-		//to do actions
-		Nodedata b1=new Nodedata(new Point3D(100, 100),0);
-		Nodedata b2=new Nodedata(new Point3D(250, 200),1);
-		Nodedata b3=new Nodedata(new Point3D(120, 280),2);
-		Nodedata b4=new Nodedata(new Point3D(350, 400),3);
-		g0.addNode(b1);
-		g0.addNode(b2);
-		g0.addNode(b3);
-		g0.addNode(b4);
-		g0.connect(0,1,0);
-		g0.connect(0,2,2);
-		g0.connect(0,3,4);		
-	}
-	
-
 }
